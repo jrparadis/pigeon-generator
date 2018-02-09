9729088820b8a29269e8bf72ea3b80e870fabecf
jrparadis <mrsithael@gmail.com>
2018-02-09 15:17:35 -0800

    Update pigeons.py

--- pigeons.py
+++ pigeons.py
@@ -20,4 +20,4 @@ x = pigeon(colors[random.randrange(0,len(colors))],
 print ('a {} {} pigeon with {} {} pattern, named {}'.format(data.personal.gender(0,1),x.color1,x.color2,x.pattern,x.name))
 print ('they have this to say: \n\n{}: {}'.format(x.name, data.text.quote()))
 print ('they drive a {} {}'.format(data.datetime.year(1935,2018),data.transport.car()))
-print ('{} runs {}'.format(x.name, data.business.company(),data.business.company_type()))
+print ('{} runs {}'.format(x.name, data.business.company()))
